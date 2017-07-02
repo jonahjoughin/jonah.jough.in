299dcd98d3aa813c8aba31c5794df7f4feb8d01d
Jonah Joughin <jonah@jough.in>
2017-07-02 11:25:37 -0400

    Added index page, body and meta components

---
+++ components/Meta.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default () => (
+  <Head>
+    <title>Joughin</title>
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+  </Head>
+)
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Meta from  '../components/Meta.js'
+import Body from '../components/Body.js'
+
+export default () => (
+  <div>
+    <Meta />
+    <Body />
+  </div>
+)
