771f72472a56d686bcc8339a6318d87bbd31f556
Jonah Joughin <jonah@jough.in>
2017-07-02 12:25:49 -0400

    Added reset style

--- components/Meta.js
+++ components/Meta.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { reset } from '../styles/default.js'
 import Head from 'next/head';
 
 export default () => (
-  <Head>
-    <title>Joughin</title>
-    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-  </Head>
+    <Head>
+      <style> { reset } </style>
+      <title>Joughin</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
 )
