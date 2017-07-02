e94d2dcaad7c8d81859b50edc53b7df4a7220c4d
Jonah Joughin <jonah@jough.in>
2017-07-02 14:41:54 -0400

    Added Header component

---
+++ components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default () => (
+  <div className="root">
+      <img src="../static/img/logo.png"/>
+
+
+  <style jsx>{`
+    .root {
+      background-color: #222;
+      height: 64px;
+      width: 100vw;
+    }
+    img {
+      // margin: auto;
+      margin-left: 6px;
+      height: 64px;
+      width: 64px;
+    }
+  `}</style>
+</div>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
+
 import Meta from  '../components/Meta.js'
+import Header from '../components/Header.js'
 import Body from '../components/Body.js'
 
 export default () => (
   <div>
     <Meta />
+    <Header />
     <Body />
   </div>
 )
