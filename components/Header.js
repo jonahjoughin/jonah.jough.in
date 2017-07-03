9fd08d29331bde357c4fb965c736550003c1d420
Jonah Joughin <jonah@jough.in>
2017-07-03 12:00:30 -0400

    Added nav bar

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import NavBar from './NavBar.js'
+import NavItem from './NavItem.js'
 
 export default () => (
   <div className="root">
       <img src="../static/img/logo.png"/>
+      <div className = "navContainer">
+        <NavBar>
+          <NavItem name="About"/>
+          <NavItem name="Blog"/>
+          <NavItem name="Contact"/>
+          <NavItem name="Home"/>
+        </NavBar>
+      </div>
 
 
   <style jsx>{`
     .root {
-      background-color: #222;
+      background-color: #FFF;
       height: 64px;
       width: 100vw;
+    }
+    .navContainer {
+      position: absolute;
+      right: 0px;
+      top: 0px;
+      height: 64px;
     }
     img {
       // margin: auto;
