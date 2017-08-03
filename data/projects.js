04d50f3ec16a223f6c1bf0ac079f9b3d722b0fb6
Jonah Joughin <jonah@Jonahs-MBP.lan>
2017-08-03 12:39:21 -0700

    Added link

--- data/projects.js
+++ data/projects.js
@@ -4,31 +4,36 @@ const LARGE = 500;
 
 export default [
   {
+    _id:1,
     height: MEDIUM,
-    color: "#841EFE",
-    borderColor: "#841EFE",
-    title: "Hungry at NYUAD",
-    i:1,
+    color: '#841EFE',
+    borderColor: '#841EFE',
+    title: 'Hungry at NYUAD',
     imageURL: '/static/images/hungryatnyuad.png',
     link: 'https://github.com/robwayne/hungrynyuad'
   },
   {
+    _id:2,
     height: SMALL,
-    backgroundColor: "#2CC853",
-    title: "Communications Lab Portfolio",
-    i:2, imageURL: '/static/images/link.png',
-    link: "https://commlab.jough.in"
+    backgroundColor: '#2CC853',
+    title: 'Communications Lab Portfolio',
+    i:2,
+    imageURL: '/static/images/link.png',
+    link: 'https://commlab.jough.in'
   },
   {
+    _id:3,
     height: LARGE,
-    backgroundColor: "#FF6A6A",
-    title: "Noordin Terrorist Network Analysis",
-    i:3, imageURL: '/static/images/graph.png'
+    backgroundColor: '#FF6A6A',
+    title: 'Noordin Terrorist Network Analysis',
+    imageURL: '/static/images/graph.png',
+    link: 'https://github.com/jonahjoughin/noordin',
   },
   {
+    _id:4,
     height: MEDIUM,
-    backgroundColor: "#841EFE",
-    title: "This Page", i:4,
+    backgroundColor: '#841EFE',
+    title: 'This Page',
     imageURL: '/static/images/jonahjoughin.png',
     link: 'https://github.com/jonahjoughin/jonah.jough.in'
   },
